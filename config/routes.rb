f666fcc8b91c0c8460b35482015c6a97f2ff5f9c
Brett Turing <bjsherman80@gmail.com>
2020-10-27 13:00:44 -0600

    added users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'welcome#index'
+  #users
+  get '/register', to: "users#new"
+  post '/register', to: "users#create"
+  get '/profile', to: 'users#show'
+  #merchants
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
   get "/merchants/:id", to: "merchants#show"
