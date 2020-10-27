3bf03ecf2eebadaa9577ef8783c461902a9d1e3e
Brett Turing <bjsherman80@gmail.com>
2020-10-27 15:41:04 -0600

    added to the update method for missing fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
+  validates :password, confirmation: { case_sensitive: true }
   validates_presence_of :name, :address, :city, :state, :zip
 
   has_secure_password
