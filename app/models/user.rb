f62aba21020c8b4b70be1ebcda3c4364dc047759
Eduardo Parra <helloeduardo@icloud.com>
2020-10-31 13:56:10 -0600

    add has_many orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
   validates :password, confirmation: { case_sensitive: true }
@@ -8,6 +7,8 @@ class User < ApplicationRecord
   enum role: [:default_user, :merchant_user, :admin_user]
 
   has_secure_password
+
+  has_many :orders
 
   def duplicate_email?
     User.pluck(:email).include?(email)
