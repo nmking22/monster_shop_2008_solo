ca69c69367679bf3db7d2325fde0458bd349c815
Brian Liu <brian.b.liu@gmail.com>
2020-10-31 17:40:24 -0600

    Create merchant items controller.

---
+++ app/controllers/merchant/items_controller.rb
@@ -0,0 +1,11 @@
+# class Merchant::ItemsController < Merchant::BaseController
+#   before_action :index
+#
+#   def index
+#     binding.pry
+#     @merchant = current_user.merchant
+#     @items = @merchant.items
+#
+#     redirect_to '/items'
+#   end
+# end
