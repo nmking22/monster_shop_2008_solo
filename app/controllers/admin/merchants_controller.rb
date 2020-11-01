a1df8f6413a0b09e7a2de0303c5b6e21c11d4c28
Nick King <nickmaxking@gmail.com>
2020-11-01 13:42:48 -0700

    Update admin merchant update action to deactivate merchant items

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -11,6 +11,9 @@ class Admin::MerchantsController < Admin::BaseController
   def update
     @merchant = Merchant.find(params[:id])
     @merchant.update(enabled: false)
+    @merchant.items.each do |item|
+      item.update(active?: false)
+    end
     redirect_to '/merchants', notice: "#{@merchant.name} has been disabled."
   end
 end
