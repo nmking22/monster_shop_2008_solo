60638ed4f648f14149752be96e04d148f12f82da
Nick King <nickmaxking@gmail.com>
2020-11-01 13:27:07 -0700

    Add test for admin merchant disable functionality

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -34,7 +34,8 @@ RSpec.describe 'merchant index page', type: :feature do
         state: 'CO',
         zip: 80210
       )
-
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      @dog_shop = Merchant.create(name: "Meg's Dog Shop", address: '123 Dog Rd.', city: 'Hershey', state: 'PA', zip: 80203)
       @user = User.create!(
         name: "Catman",
         address: "Some Straw Hole",
@@ -55,6 +56,22 @@ RSpec.describe 'merchant index page', type: :feature do
       expect(current_path).to eq("/merchants/#{@cat_shop.id}")
 
       expect(page).to have_content(@cat_shop.address)
+    end
+    describe "When I visit '/admin/merchants' and click on a 'Disable' button" do
+      it "I am returned to '/admin/merchants' with that merchant disabled and a flash message" do
+        visit '/admin/merchants'
+        within "#merchant-#{@cat_shop.id}" do
+          click_button 'Disable Merchant'
+        end
+
+        expect(current_path).to eq('/merchants')
+
+        within "#merchant-#{@cat_shop.id}" do
+          expect(page).to have_content('Disabled')
+        end
+
+        expect(page).to have_content("#{@cat_shop.name} has been disabled.")
+      end
     end
   end
 end
