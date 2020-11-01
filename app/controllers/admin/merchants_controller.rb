e41121ee0ecbe34e5e5dbefe146f3b7ed5619f6b
Nick King <nickmaxking@gmail.com>
2020-11-01 13:28:14 -0700

    Add index and update actions

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,6 +1,16 @@
 class Admin::MerchantsController < Admin::BaseController
+  def index
+    redirect_to '/merchants'
+  end
+
   def show
-    @merchant = Merchant.find(params[:merchant_id])
+    @merchant = Merchant.find(params[:id])
     redirect_to "/merchants/#{@merchant.id}"
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(enabled: false)
+    redirect_to '/merchants', notice: "#{@merchant.name} has been disabled."
   end
 end
