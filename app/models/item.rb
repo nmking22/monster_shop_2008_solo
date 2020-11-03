f2a50f4969294dbbb996870d1fd18970a0fd5b25
Brian Liu <brian.b.liu@gmail.com>
2020-11-03 12:17:45 -0700

    Allow blank for image in item model.

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,8 @@ class Item <ApplicationRecord
   validates_presence_of :name,
                         :description,
                         :price,
-                        :image,
                         :inventory
+  validates_presence_of :image, :allow_blank => true
   validates_inclusion_of :active?, :in => [true, false]
   validates_numericality_of :price, greater_than: 0
 
