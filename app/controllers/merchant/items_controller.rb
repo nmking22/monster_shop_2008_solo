f5f1f3bb8ca01f524368862ce3b3881d9cf5af4a
Eduardo Parra <helloeduardo@icloud.com>
2020-11-02 18:55:15 -0700

    Add update method to merchant items controller

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,10 +1,22 @@
 class Merchant::ItemsController < Merchant::BaseController
 
   def index
-    # binding.pry
-    # @merchant = current_user.merchant
-    # @items = @merchant.items
-    #
-    redirect_to '/items'
+    user = User.find(current_user.id)
+    @merchant = user.merchant
+    @items = @merchant.items
+    # redirect_to '/items'
+  end
+
+  def update
+    item = Item.find(params[:id])
+    # US 47 might be impacted by the below, merchant editing items
+    if item.active?
+      item.update(active?: false)
+      flash[:notice] = "The item is no longer for sale"
+    else
+      item.update(active?: true)
+      flash[:notice] = "The item is for sale"
+    end
+    redirect_to "/merchant/items"
   end
 end
