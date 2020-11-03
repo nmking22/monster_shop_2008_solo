c1f4cf09b94934f4828f1a6b5df754186962aa41
Brett Turing <bjsherman80@gmail.com>
2020-11-03 10:45:44 -0700

    added item_order route to merchant namespace

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,7 @@ Rails.application.routes.draw do
   namespace :merchant do
     get '/', to: 'dashboard#show'
     get '/items', to: 'items#index'
+    resources :item_orders, only: [:update]
     resources :orders, only: [:show]
   end
 
