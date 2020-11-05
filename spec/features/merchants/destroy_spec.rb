a1c6bde88b7c9f0e7276c9ef65f86b992ecda3ce
Brian Liu <brian.b.liu@gmail.com>
2020-11-05 15:59:51 -0700

    Revise merchants destroy spec to update user priviliges

--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -1,28 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor" do
-  describe "When I visit a merchant show page" do
-    it "I can delete a merchant" do
+RSpec.describe "When I visit a merchant show page" do
+  describe "As a visitor" do
+    it "I cannot delete a merchant" do
       bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
 
       visit "merchants/#{bike_shop.id}"
-
-      click_on "Delete Merchant"
-
-      expect(current_path).to eq('/merchants')
-      expect(page).to_not have_content("Brian's Bike Shop")
+      expect(page).to_not have_content("Delete Merchant")
     end
 
-    it "I can delete a merchant that has items" do
-      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    describe "As an admin" do
+      it "I can delete a merchant even if they have items" do
+        user = User.create!(name: "Batman",
+                              address: "Some dark cave 11",
+                              city: "Arkham",
+                              state: "CO",
+                              zip: "81301",
+                              email: 'batmansemail@email.com',
+                              password: "password",
+                              role: 2)
 
-      visit "merchants/#{bike_shop.id}"
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      click_on "Delete Merchant"
+        bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+        chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
 
-      expect(current_path).to eq('/merchants')
-      expect(page).to_not have_content("Brian's Bike Shop")
+        visit "merchants/#{bike_shop.id}"
+        click_on "Delete Merchant"
+
+        expect(current_path).to eq('/merchants')
+        expect(page).to_not have_content("Brian's Bike Shop")
+      end
     end
 
     describe "As a registered user" do
@@ -74,9 +82,6 @@ RSpec.describe "As a visitor" do
 
         visit "/merchants/#{meg.id}"
         expect(page).to_not have_link("Delete Merchant")
-
-        # visit "/merchants/#{brian.id}"
-        # expect(page).to have_link("Delete Merchant")
       end
     end
   end
