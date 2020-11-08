13b695ddf604bde140e268c2b8c5adfedffd8c9f
Nick King <nickmaxking@gmail.com>
2020-11-08 14:32:26 -0700

    Add discount show, edit and update actions

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -15,6 +15,20 @@ class Merchant::DiscountsController < Merchant::BaseController
     redirect_to '/merchant/discounts'
   end
 
+  def show
+    @discount = Discount.find(params[:id])
+  end
+
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    discount = Discount.find(params[:id])
+    discount.update!(discount_params)
+    redirect_to "/merchant/discounts/#{discount.id}"
+  end
+
   private
   def discount_params
     params.permit(:name, :percentage, :threshold)
