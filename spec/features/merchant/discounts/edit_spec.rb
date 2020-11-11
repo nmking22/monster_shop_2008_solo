144fd97dc3f170d7fe642ed2862af549997828b5
Nick King <nickmaxking@gmail.com>
2020-11-10 22:34:27 -0700

    Add sad path and edge case testing for edit form

--- spec/features/merchant/discounts/edit_spec.rb
+++ spec/features/merchant/discounts/edit_spec.rb
@@ -45,12 +45,118 @@ describe 'As a Merchant Employee, when I visit a discount edit page' do
       expect(page).to have_content("Minimum Eligible Quantity: 11")
     end
   end
-  
+
   it 'The form fields are pre-filled with current attributes' do
     visit "/merchant/discounts/#{@ten_rangs.id}"
 
     expect(page).to have_content(@ten_rangs.name)
     expect(page).to have_content(@ten_rangs.percentage)
     expect(page).to have_content(@ten_rangs.threshold)
+  end
+
+  describe 'When I fill out the discounts edit form with a missing name field' do
+    it 'I am redirected back to discounts edit with a flash message indicating the problem' do
+      visit "/merchant/discounts/#{@ten_rangs.id}/edit"
+
+      fill_in :name, with: ""
+      fill_in :percentage, with: 5
+      fill_in :threshold, with: 20
+
+      click_button 'Update Discount'
+
+      expect(current_path).to eq("/merchant/discounts/#{@ten_rangs.id}/edit")
+      expect(page).to have_content('All fields must be filled in.')
+    end
+  end
+
+  describe 'When I fill out the discounts edit form with a missing percentage field' do
+    it 'I am redirected back to discounts edit with a flash message indicating the problem' do
+      visit "/merchant/discounts/#{@ten_rangs.id}/edit"
+
+      fill_in :name, with: "Nick!"
+      fill_in :percentage, with: ""
+      fill_in :threshold, with: 20
+
+      click_button 'Update Discount'
+
+      expect(current_path).to eq("/merchant/discounts/#{@ten_rangs.id}/edit")
+      expect(page).to have_content('Discount percentage must be filled with an integer or float between 0 and 100.')
+    end
+  end
+
+  describe 'When I fill out the discounts edit form with a missing threshold field' do
+    it 'I am redirected back to discounts edit with a flash message indicating the problem' do
+      visit "/merchant/discounts/#{@ten_rangs.id}/edit"
+
+      fill_in :name, with: "Nick!"
+      fill_in :percentage, with: 5.5
+      fill_in :threshold, with: ""
+
+      click_button 'Update Discount'
+
+      expect(current_path).to eq("/merchant/discounts/#{@ten_rangs.id}/edit")
+      expect(page).to have_content('Minimum Items for Activation must be filled with an integer zero or greater.')
+    end
+  end
+
+  describe "When I fill in the discounts edit form with a negative percentage" do
+    it 'I am redirected to discounts edit with an error indicating an invalid percentage' do
+      visit "/merchant/discounts/#{@ten_rangs.id}/edit"
+
+      fill_in :name, with: "Nick!"
+      fill_in :percentage, with: -5
+      fill_in :threshold, with: 20
+
+      click_button 'Update Discount'
+
+      expect(current_path).to eq("/merchant/discounts/#{@ten_rangs.id}/edit")
+      expect(page).to have_content('Discount percentage must be filled with an integer or float between 0 and 100.')
+    end
+  end
+
+  describe "When I fill in the discounts edit form with a percentage over 100" do
+    it 'I am redirected to discounts edit with an error indicating an invalid percentage' do
+      visit "/merchant/discounts/#{@ten_rangs.id}/edit"
+
+      fill_in :name, with: "Nick!"
+      fill_in :percentage, with: 100.1
+      fill_in :threshold, with: 20
+
+      click_button 'Update Discount'
+
+      expect(current_path).to eq("/merchant/discounts/#{@ten_rangs.id}/edit")
+      expect(page).to have_content('Discount percentage must be filled with an integer or float between 0 and 100.')
+    end
+  end
+
+  describe "When I fill in the discounts edit form with a negative threshold input" do
+    it 'I am redirected to discounts edit with an invalid threshold message' do
+      visit "/merchant/discounts/#{@ten_rangs.id}/edit"
+
+      fill_in :name, with: "Nick!"
+      fill_in :percentage, with: 20
+      fill_in :threshold, with: -23
+
+      click_button 'Update Discount'
+
+      expect(current_path).to eq("/merchant/discounts/#{@ten_rangs.id}/edit")
+      expect(page).to have_content('Minimum Items for Activation must be filled with an integer zero or greater.')
+    end
+  end
+
+  describe "When I fill in the discounts edit form with a float threshold input" do
+    it 'I am redirected to that discounts show with threshold rounded down' do
+      visit "/merchant/discounts/#{@ten_rangs.id}/edit"
+
+      fill_in :name, with: "Nick!"
+      fill_in :percentage, with: 20
+      fill_in :threshold, with: 20.92
+
+      click_button 'Update Discount'
+
+      expect(current_path).to eq("/merchant/discounts/#{@ten_rangs.id}")
+
+      expect(page).to have_content('Minimum Eligible Quantity: 20')
+    end
   end
 end
