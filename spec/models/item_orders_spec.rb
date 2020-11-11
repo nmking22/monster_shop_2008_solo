feae5418ad288512704f88ecec11e6deb9bc0d2b
Nick King <nickmaxking@gmail.com>
2020-11-10 22:55:53 -0700

    Remove unnecessary item_order/discount relationship and tests

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -11,7 +11,6 @@ describe ItemOrder, type: :model do
   describe "relationships" do
     it { should belong_to :item }
     it { should belong_to :order }
-    it { should belong_to(:discount).optional }
   end
 
   describe 'instance methods' do
