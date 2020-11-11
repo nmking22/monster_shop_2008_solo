40e9746f9d1deead34ca4dc5c38812d193cb89db
Nick King <nickmaxking@gmail.com>
2020-11-10 21:12:11 -0700

    Add test for invalid_percentage instance method

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -12,4 +12,31 @@ describe Discount, type: :model do
     it { should belong_to :merchant }
     it { should have_many :item_orders }
   end
+
+  describe 'instance_methods' do
+    it '#invalid_percentage?' do
+      batarang_emporium = Merchant.create(
+        name: "Batman's Batarang Emporium",
+        address: '123 Wayne Dr.',
+        city: 'Gotham',
+        state: 'IL',
+        zip: 80210
+      )
+      ten_rangs = Discount.new(
+        name: 'Ten Rangs',
+        percentage: -5,
+        threshold: 10,
+        merchant: @batarang_emporium
+      )
+      hero_discount = Discount.new(
+        name: 'Hero Discount',
+        percentage: 101,
+        threshold: 20,
+        merchant: @batarang_emporium
+      )
+
+      expect(ten_rangs.invalid_percentage?).to eq(true)
+      expect(hero_discount.invalid_percentage?).to eq(true)
+    end
+  end
 end
