ffa23417fe4d95d00da4c0f2344f904b88ae546c
Nick King <nickmaxking@gmail.com>
2020-11-10 21:42:18 -0700

    Reorder create action conditional to avoid unwanted saves

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -11,10 +11,12 @@ class Merchant::DiscountsController < Merchant::BaseController
   def create
     discount = Discount.new(discount_params)
     discount.merchant = current_user.merchant
-    if !discount.save
-      redirect_to '/merchant/discounts/new', notice: 'All fields must be filled in.'
+    if discount.invalid_threshold?
+      redirect_to '/merchant/discounts/new', notice: 'Minimum Items for Activation must be filled with an integer zero or greater.'
     elsif discount.invalid_percentage?
-      redirect_to '/merchant/discounts/new', notice: 'Discount percentage must be an integer or float between 0 and 100.'
+      redirect_to '/merchant/discounts/new', notice: 'Discount percentage must be filled with an integer or float between 0 and 100.'
+    elsif !discount.save
+      redirect_to '/merchant/discounts/new', notice: 'All fields must be filled in.'
     else
       redirect_to '/merchant/discounts'
     end
